6212e44b0c766e9cfb7087ca447de54b5c40d413
Dustin Wise <dustinadwise@Dustins-MacBook-Pro.local>
2024-09-01 03:42:38 -0400

    Updated names on public subnets and created private subnets.

--- Modules/Network/main.tf
+++ Modules/Network/main.tf
@@ -19,7 +19,7 @@ resource "aws_vpc" "GamingConsole_VPC" {
 
 
 
-# Public Subnet 1 in Development
+# Public Subnet 1
 resource "aws_subnet" "public_subnet1" {
     vpc_id = aws_vpc.GamingConsole_VPC.id
     cidr_block = "10.0.1.0/24"
@@ -38,5 +38,55 @@ resource "aws_subnet" "public_subnet2" {
 
     tags = {
         Name = "GamingConsole_Public_Subnet2"
+    }
+}
+
+
+# Private Application Subnet 1
+resource "aws_subnet" "private_app_subnet1" {
+    vpc_id = aws_vpc.GamingConsole_VPC.id
+    cidr_block = "10.0.3.0/24"
+    availability_zone = "us-east-1a"
+
+    tags = {
+        Name = "GamingConsole_Private_Subnet1"
+    }
+}
+
+
+# Private Application Subnet 2
+resource "aws_subnet" "private_app_subnet2" {
+    vpc_id = aws_vpc.GamingConsole_VPC.id
+    cidr_block = "10.0.4.0/24"
+    availability_zone = "us-east-1b"
+
+    tags = {
+        Name = "GamingConsole_Private_Subnet2"
+    }
+}
+
+
+
+# Private Database Subnet 1
+resource "aws_subnet" "private_db_subnet1" {
+    vpc_id = aws_vpc.GamingConsole_VPC.id
+    cidr_block = "10.0.5.0/24"
+    availability_zone = "us-east-1a"
+
+    tags = {
+        Name = "GamingConsole_Private_Subnet3"
+    }
+}
+
+
+
+# Private Database Subnet 2
+resource "aws_subnet" "private_db_subnet2" {
+    vpc_id = aws_vpc.GamingConsole_VPC.id
+    cidr_block = "10.0.6.0/24"
+    availability_zone = "us-east-1b"
+
+    tags = {
+        Name = "GamingConsole_Private_Subnet4"
     }
 }
