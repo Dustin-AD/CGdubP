b2c38acf9213793db375b4f03bbe5222390cab79
Dustin Wise <dustinadwise@Dustins-MacBook-Pro.local>
2024-09-01 02:58:59 -0400

    Updated README.md with project overview.

--- Modules/Network/main.tf
+++ Modules/Network/main.tf
@@ -20,23 +20,23 @@ resource "aws_vpc" "GamingConsole_VPC" {
 
 
 # Public Subnet 1 in Development
-resource "aws_subnet" "dev_public_subnet1" {
+resource "aws_subnet" "public_subnet1" {
     vpc_id = aws_vpc.GamingConsole_VPC.id
     cidr_block = "10.0.1.0/24"
     availability_zone = "us-east-1b"
 
     tags = {
-        Name = "GamingConsole_Dev_Public_Subnet1"
+        Name = "GamingConsole_Public_Subnet1"
     }
 }
 
 # Public Subnet 2 in Development
-resource "aws_subnet" "dev_public_subnet2" {
+resource "aws_subnet" "public_subnet2" {
     vpc_id = aws_vpc.GamingConsole_VPC.id
     cidr_block = "10.0.2.0/24"
     availability_zone = "us-east-1a"
 
     tags = {
-        Name = "GamingConsole_Dev_Public_Subnet2"
+        Name = "GamingConsole_Public_Subnet2"
     }
 }
