6ce2b2a10ff5b8573749ba5dea0a116caf0c9e0e
Dustin Wise <dustinadwise@Dustins-MacBook-Pro.local>
2024-09-01 04:02:18 -0400

    Created IGW, Route Table, and Associations for Public Subnets.

--- Modules/Network/main.tf
+++ Modules/Network/main.tf
@@ -90,3 +90,46 @@ resource "aws_subnet" "private_db_subnet2" {
         Name = "GamingConsole_Private_Subnet4"
     }
 }
+
+
+
+# Internet Gateway
+resource "aws_internet_gateway" "internet_gateway" {
+    vpc_id = aws_vpc.GamingConsole_VPC.id
+
+    tags = {
+        Name = "GamingConsole_Internet_Gateway"
+    }
+}
+
+
+
+# Route Table for Public Subnets
+resource "aws_route_table" "public_route_table" {
+    vpc_id = aws_vpc.GamingConsole_VPC.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.internet_gateway.id
+    }
+
+    tags = {
+        Name = "GamingConsole_Public_Route_Table"
+    }
+}
+
+
+
+# Associate Public Subnet 1 with Public Route Table
+resource "aws_route_table_association" "public_subnet1_association" {
+    subnet_id = aws_subnet.public_subnet1.id
+    route_table_id = aws_route_table.public_route_table.id
+}
+
+
+
+# Associate Public Subnet 2 with Public Route Table
+resource "aws_route_table_association" "public_subnet2_association" {
+    subnet_id = aws_subnet.public_subnet2.id
+    route_table_id = aws_route_table.public_route_table.id
+}
